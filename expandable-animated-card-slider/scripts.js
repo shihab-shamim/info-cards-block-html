58a7897dfcfb92878c5b16b89d2797bbebcf3bff
shihab- hamim <shihabshamim767@gmail.com.com>
2026-03-29 14:18:04 +0600

    added news cards

--- expandable-animated-card-slider/scripts.js
+++ expandable-animated-card-slider/scripts.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
       '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg>',
       '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/></svg>',
     ],
-    // autoplay: true, // অটো চলবে
-    autoplayTimeout: 1000, // ৪ সেকেন্ড বিরতি
-    smartSpeed: 800, // স্লাইড হওয়ার গতি (মসৃণতা)
+    // autoplay: true,
+    autoplayTimeout: 1000,
+    smartSpeed: 800,
     // nav: true,
     lazyLoad: true,
     // rtl: true,
